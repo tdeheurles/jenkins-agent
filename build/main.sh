3903314c35da2068b32da9c2e1422527ee0c70de
tdeheurles <tdeheurles@gmail.com>
2015-06-16 14:13:55 +0200

    Corrected artifact name

--- build/main.sh
+++ build/main.sh
@@ -3,12 +3,12 @@
 # import names
 . ./build/release.cfg
 artifact_name="gcr.io/$projectid/$servicename"
-artifact_version="$servicemajor.$serviceminor.$BUILD_NUMBER"
+artifact_tag="$artifact_name:$servicemajor.$serviceminor.$BUILD_NUMBER"
 
 # Build
 docker build -t $artifact_name .
-docker tag $artifact_name $artifact_name:$artifact_version
+docker tag $artifact_tag
 
 # Push to Google Cloud Engine
-gcloud preview docker push $artifactname_build
-gcloud preview docker push $artifactname_latest
+gcloud preview docker push $artifact_name
+gcloud preview docker push $artifact_tag
